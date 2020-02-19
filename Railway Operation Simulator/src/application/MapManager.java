dcbd8ffa2dd6df90d43df4345c571aca07e81b92
jkwok678 <jkwok678@gmail.com>
2020-02-19 11:24:17 +0000

    Update MapManager.java
    Added singleton design to the mapmanager

--- Railway Operation Simulator/src/application/MapManager.java
+++ Railway Operation Simulator/src/application/MapManager.java
@@ -1,5 +1,50 @@
 package application;
 
 public class MapManager {
+	
+	
+	private static MapManager onlyMapManager;
+	private Map map;
+
+	/**
+	 * Empty MapManager constructor.
+	 */
+	private MapManager() {
+
+	}
+
+	/**
+	 * Make a map manager.
+	 * 
+	 * @return a mapManager object
+	 */
+	public static MapManager sharedMapManager() {
+
+		if (onlyMapManager == null) {
+			onlyMapManager = new MapManager();
+		}
+		return onlyMapManager;
+	}
+
+	/**
+	 * Return the map.
+	 * 
+	 * @return map of game
+	 */
+	public Map getMap() {
+		return this.map;
+	}
+
+	/**
+	 * Set the map.
+	 * 
+	 * @param map of game
+	 */
+	public void setMap(Map map) {
+		this.map = map;
+	}
+
+}
+
 
 }
