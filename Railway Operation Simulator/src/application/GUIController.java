ce6bd61e35b1bc01abb67f0ef398622eaf6cd7cf
jkwok678 <jkwok678@gmail.com>
2020-02-19 19:24:42 +0000

    Added shortcut for Help and Exit
    Alt-F4 for exit and F1 for Help( Doesn't do anything yet)

--- Railway Operation Simulator/src/application/GUIController.java
+++ Railway Operation Simulator/src/application/GUIController.java
@@ -10,6 +10,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class GUIController extends Application {
 
     @FXML // ResourceBundle that was given to the FXMLLoader
@@ -18,11 +23,26 @@ public class GUIController extends Application {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // fx:id="RailwayHelp"
-    private MenuItem RailwayHelp; // Value injected by FXMLLoader
+    @FXML // fx:id="ExitMenuItem"
+    private MenuItem ExitMenuItem; // Value injected by FXMLLoader
 
-    @FXML // fx:id="RailwayWebsite"
-    private MenuItem RailwayWebsite; // Value injected by FXMLLoader
+    @FXML // fx:id="RailwayHelpMenuItem"
+    private MenuItem RailwayHelpMenuItem; // Value injected by FXMLLoader
+
+    @FXML // fx:id="RailwayWebsiteMenuItem"
+    private MenuItem RailwayWebsiteMenuItem; // Value injected by FXMLLoader
+
+    @FXML // fx:id="AboutMenuItem"
+    private MenuItem AboutMenuItem; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert ExitMenuItem != null : "fx:id=\"ExitMenuItem\" was not injected: check your FXML file 'GUI.fxml'.";
+        assert RailwayHelpMenuItem != null : "fx:id=\"RailwayHelpMenuItem\" was not injected: check your FXML file 'GUI.fxml'.";
+        assert RailwayWebsiteMenuItem != null : "fx:id=\"RailwayWebsiteMenuItem\" was not injected: check your FXML file 'GUI.fxml'.";
+        assert AboutMenuItem != null : "fx:id=\"AboutMenuItem\" was not injected: check your FXML file 'GUI.fxml'.";
+
+    }
     
     @FXML
     void exitApp(ActionEvent event) {
@@ -40,16 +60,11 @@ public class GUIController extends Application {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert RailwayHelp != null : "fx:id=\"RailwayHelp\" was not injected: check your FXML file 'GUI.fxml'.";
-        assert RailwayWebsite != null : "fx:id=\"RailwayWebsite\" was not injected: check your FXML file 'GUI.fxml'.";
-
-    }
-
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 }
+
+
